67c2080179ec143455e7009a2e0c2c2d5c9a6339
Wander <14371266+Datou86@users.noreply.github.com>
2020-08-06 19:25:29 +0800

    Update XiaoMoNv.js

--- Script/XiaoMoNv.js
+++ Script/XiaoMoNv.js
@@ -4,7 +4,14 @@ https://sf6.xyz/?oem=7506bab5fe133526
 TG频道:https://t.me/liangyiA
 [rewrite_local]
 #小魔女破解付费房
-^http:\/\/m\.xmn\d+\.com\/xmn\/live\/room\/intoRoom\/* url script-response-body XiaoMoNv.js
+^http:\/\/m\.xmn\d+\.com\/xmn\/live\/room\/intoRoom\/* url script-response-body https://raw.githubusercontent.com/Datou86/Quantumult-X/master/Script/XiaoMoNv.js
+^http:\/\/m\.xmn\d+\.com\/xmn\/live\/room\/honoureds\/* url script-response-body https://raw.githubusercontent.com/Datou86/Quantumult-X/master/Script/XiaoMoNv.js
+^http:\/\/m\.hahuiyun\.com\/xmn\/live\/room\/intoRoom\/* url script-response-body https://raw.githubusercontent.com/Datou86/Quantumult-X/master/Script/XiaoMoNv.js
+^http:\/\/m\.hahuiyun\.com\/xmn\/live\/room\/honoureds\/* url script-response-body https://raw.githubusercontent.com/Datou86/Quantumult-X/master/Script/XiaoMoNv.js
+^http:\/\/m\.0575myqc\.com\/xmn\/live\/room\/intoRoom\/* url script-response-body https://raw.githubusercontent.com/Datou86/Quantumult-X/master/Script/XiaoMoNv.js
+^http:\/\/m\.0575myqc\.com\/xmn\/live\/room\/honoureds\/* url script-response-body https://raw.githubusercontent.com/Datou86/Quantumult-X/master/Script/XiaoMoNv.js
+^http:\/\/m\.typaimai\.com\/xmn\/live\/room\/intoRoom\/* url script-response-body https://raw.githubusercontent.com/Datou86/Quantumult-X/master/Script/XiaoMoNv.js
+^http:\/\/m\.typaimai\.com\/xmn\/live\/room\/honoureds\/* url script-response-body https://raw.githubusercontent.com/Datou86/Quantumult-X/master/Script/XiaoMoNv.js
 hostname = m.*.com
 */
 let obj = JSON.parse($response.body);
